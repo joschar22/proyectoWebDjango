1cb370c24c54dd3867b4e2a0f0e4907be4accf55
José <chaqquererj@gmail.com>
2023-01-20 03:25:04 -0500

    carrito completado

--- ProydtoWeb/ProydtoWeb/urls.py
+++ ProydtoWeb/ProydtoWeb/urls.py
@@ -19,11 +19,15 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('ProyectoWebApp.urls')),
+  
     
     path('servicios/', include("servicios.urls")),
     path('blog/', include("blog.urls")),
     path('contacto/', include("contacto.urls")),
     path('tienda/', include("tienda.urls")),
+    path('carro/', include("carro.urls")),
+
+
+    path('',include('ProyectoWebApp.urls')),
  
 ]
